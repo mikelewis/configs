fb9d38f27a9a0dc823dba73380628f285211c4ea
Mike Lewis <ft.mikelewis@gmail.com>
2013-06-09 18:19:51 -0700

    create empty file

--- setup_configs.sh
+++ setup_configs.sh
@@ -6,4 +6,5 @@ cd  ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/
 ln -sf ~/configs/sublime/Preferences.sublime-settings Preferences.sublime-settings
 cd  ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/Color\ Scheme\ -\ Default/
 ln -sf ~/configs/sublime/Tomorrow-Night.tmTheme Tomorrow-Night.tmTheme
+echo "" > ~/configs/zsh/machine_zsh_configs/empty.zsh
 cd ~
