260ca2f8e8dfc1b7cd20c5a3af3a0195db13b55d
Mike Lewis <mlewis@scribd.com>
2013-06-09 18:04:33 -0700

    ack

--- setup_configs.sh
+++ setup_configs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd ~
 ln -sf ~/configs/zsh/.zshrc .zshrc
+ln -sf ~/configs/ack/.ackrc .ackrc
 cd  ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/
 ln -sf ~/configs/sublime/Preferences.sublime-settings Preferences.sublime-settings
 cd  ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/Color\ Scheme\ -\ Default/
