684c88c58f641806ef4792b6fcfc289c37284bfd
Mike Lewis <mlewis@scribd.com>
2013-06-09 17:58:31 -0700

    sublime

--- setup_configs.sh
+++ setup_configs.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 cd ~
-ln -s configs/.zshrc .zshrc
+ln -sf configs/.zshrc .zshrc
+cd  ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/
+ln -sf ~/configs/sublime/Preferences.sublime-settings Preferences.sublime-settings
+cd  ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/Color\ Scheme\ -\ Default/
+ln -sf ~/configs/sublime/Tomorrow-Night.tmTheme Tomorrow-Night.tmTheme
+cd ~
