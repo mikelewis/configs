b9ced263e32f2c224b2abeb9526a780276768242
Mike Lewis <mikelewis@Mikes-MacBook-Pro.local>
2013-11-15 14:59:16 -0800

    Sublime 3

--- setup_configs.sh
+++ setup_configs.sh
@@ -2,9 +2,10 @@
 cd ~
 ln -sf ~/configs/zsh/.zshrc .zshrc
 ln -sf ~/configs/ack/.ackrc .ackrc
-cd  ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/
+cd  ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
 ln -sf ~/configs/sublime/Preferences.sublime-settings Preferences.sublime-settings
-cd  ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/Color\ Scheme\ -\ Default/
+mkdir  ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/TomorrowNight
+cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/TomorrowNight/
 ln -sf ~/configs/sublime/Tomorrow-Night.tmTheme Tomorrow-Night.tmTheme
 echo "" > ~/configs/zsh/machine_zsh_configs/empty.zsh
 cd ~
